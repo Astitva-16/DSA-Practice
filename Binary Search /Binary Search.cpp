c099cfc39cd86fca0d1ed9ffb48f48825c1c4cd6
Astitva Garg <astitva1216@gmail.com>
2026-04-03 23:21:05 +0530

    Binary Search

---
+++ Binary Search /Binary Search.cpp
@@ -0,0 +1,29 @@
+/*
+Problem - Binary Search
+Leetcode - https://leetcode.com/problems/binary-search/
+*/
+
+//APPROACH - Binary Search
+//Time Complexity - O(log n)
+//Space Complexity - O(1)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int low = 0, high = n - 1;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target) return mid;
+
+            else if(nums[mid] < target) low = mid + 1;
+
+            else high = mid - 1;
+        }
+
+        return -1;
+    }
+};
